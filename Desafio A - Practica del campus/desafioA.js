afb5e0cf22c3ba328b3122d5b919c70dbbd7ae76
Mauro Bringas <maurosbringas@gmail.com>
2020-10-18 21:39:29 -0300

    Nivel 2 terminado

---
+++ Desafio A - Practica del campus/desafioA.js
@@ -0,0 +1,46 @@
+
+// 1.- Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
+// alert('un mensaje')
+
+// 2.- Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).
+// document.write('hello world')
+
+// 3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+// document.write(3+5)
+
+// 4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+// let nombreUsuario = prompt('Ingrese su nombre')
+// document.write('Hola ' + nombreUsuario)
+
+// 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+// let numeroUno = parseInt(prompt('Ingrese el primer numero'))
+// let numeroDos = parseInt(prompt('Ingrese el segundo numero'))
+// document.write(numeroUno + numeroDos)
+
+
+// 6.- Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+// let numeroUno = parseInt(prompt('Ingrese el primer numero'))
+// let numeroDos = parseInt(prompt('Ingrese el segundo numero'))
+// if (numeroUno > numeroDos) {
+//     document.write (numeroUno + ' es mayor que ' + numeroDos)
+// } else {
+//     document.write (numeroDos + ' es mayor que ' + numeroUno)
+// }
+
+// 7.- Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
+ let numeroUno = parseInt(prompt('Ingrese el primer numero'))
+let numeroDos = parseInt(prompt('Ingrese el segundo numero'))
+let numeroTres = parseInt(prompt('Ingrese el tercer numero'))
+if (numeroUno > numeroDos, numeroUno > numeroTres) {
+    document.write(NumeroUno + ' es el mayor de los 3 numeros ingresados')
+} else if 
+
+// 8.- Escribe un programa que pida un número y diga si es divisible por 2
+
+// 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+
+// Nota: Tener en cuenta la función length y substr.
+
+// 10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+ 
+// 11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
